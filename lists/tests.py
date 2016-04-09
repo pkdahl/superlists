bdc2c8af428f67dab9b285ba85e953384b87808a
pkdahl <pkdahl@gmail.com>
2016-04-10 00:13:29 +0200

    adjust model so that items belong to lists

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -42,8 +42,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        item_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=item_list)
+        Item.objects.create(text="itemey 2", list=item_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -51,17 +52,26 @@ class ListViewTest(TestCase):
         self.assertContains(response, "itemey 2")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        item_list = List()
+        item_list.save()
+
         first_item = Item()
         first_item_text = "The first (ever) list item"
         first_item.text = first_item_text
+        first_item.list = item_list
         first_item.save()
+
         second_item = Item()
         second_item_text = "Item the second"
         second_item.text = second_item_text
+        second_item.list = item_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, item_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -69,4 +79,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, first_item_text)
+        self.assertEqual(first_saved_item.list, item_list)
         self.assertEqual(second_saved_item.text, second_item_text)
+        self.assertEqual(second_saved_item.list, item_list)
